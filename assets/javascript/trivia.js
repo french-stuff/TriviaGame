5abb975d2fe81061bbed2d1fc885e69ed3d7a649
Franchesca <legarreta.french@gmail.com>
2019-10-16 14:26:43 -0400

    last commit didnt have variables. Now it does

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,4 +1,14 @@
 function check() {
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var correct = 0;
+
+
+      
+
     document.getElementById("after_submit").style.visibility = "visible"; 
 
 }
