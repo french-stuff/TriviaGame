d4a1bb0ff2623f935302bf0a3a976a393cb18ae1
Franchesca <legarreta.french@gmail.com>
2019-10-16 16:25:54 -0400

    added if statements for amount of correct answers

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,4 +1,4 @@
-function check() {
+function check(){
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
@@ -6,11 +6,24 @@ function check() {
     var question5 = document.quiz.question5.value;
     var correct = 0;
 
-
-      
+    if (question1 == 1994) {
+        correct++;
+}
+    if (question2 == 2001) {
+        correct++;
+}
+    if (question3 == 1999) {
+        correct++;
+}
+    if (question4 == 1985) {
+        correct++;
+}
+    if (question5 == 1995) {
+        correct++;
+}
 
     document.getElementById("after_submit").style.visibility = "visible"; 
-
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 }
 
 
